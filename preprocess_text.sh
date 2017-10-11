c036901074f254312e042c6ea0a73e2de17f2b5e
Benjamin Heinzerling <benjamin.heinzerling@h-its.org>
2017-10-11 22:26:27 +0200

    fix #1

--- preprocess_text.sh
+++ preprocess_text.sh
@@ -6,4 +6,4 @@ infile=$1
 outfile=${infile}.clean
 
 # replace urls with <url>, map digits to zero, collapse whitespace, lowercase
-sed -e "s#http : / / #http://#ig;s#https : / / #https://#ig;s/ ’ s / 's /ig;s/ ’ /'/g;s/-LRB-/(/g;s/-RRB-/)/g;s/http:[^\s]*/<url> /ig;s/www\.[^\s]*/<url> /ig;s/[0-9]/0/g" $f | tr -s "  " " " | tr "[:upper:]" "[:lower:]" < $infile > $outfile
+sed -e "s#http : / / #http://#ig;s#https : / / #https://#ig;s/ ’ s / 's /ig;s/ ’ /'/g;s/-LRB-/(/g;s/-RRB-/)/g;s/http:[^\s]*/<url> /ig;s/www\.[^\s]*/<url> /ig;s/[0-9]/0/g" $infile | tr -s "  " " " | tr "[:upper:]" "[:lower:]" > $outfile
