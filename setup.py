d9d546c28e9e9672d74437d6b5eb6984db35c401
Benjamin Heinzerling <benjamin.heinzerling@h-its.org>
2018-11-29 15:29:35 +0100

    fix language list

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='bpemb',
-    version='0.2.5',
+    version='0.2.8',
     description='Byte-pair embeddings in 275 languages',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='http://github.com/bheinzerling/bpemb',
     author='Benjamin Heinzerling',
     author_email='benjamin.heinzerling@h-its.org',
@@ -15,4 +20,9 @@ setup(
         "requests",
         "sentencepiece",
         "tqdm"],
-    zip_safe=True)
+    zip_safe=True,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3"
+    ])
