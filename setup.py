6885e1796831ec662e0149a716c1b1669e985846
Benjamin Heinzerling <benjamin.heinzerling@h-its.org>
2019-02-15 12:17:40 +0100

    make instances serializable without local file workaround

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='bpemb',
-    version='0.2.10',
+    version='0.2.11',
     description='Byte-pair embeddings in 275 languages',
     long_description=long_description,
     long_description_content_type="text/markdown",
