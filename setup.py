64a99d47f401c69b49dbe11600887d14ca2ebd35
KOLANICH <kolan_n@mail.ru>
2023-02-16 19:35:01 +0300

    Move the metadata into `setup.cfg` supported since Dec 2016.

--- setup.py
+++ setup.py
@@ -1,32 +1,4 @@
 from setuptools import setup
 
-with open("README.md", "r", encoding='utf-8') as fh:
-    long_description = fh.read()
-
-setup(
-    name='bpemb',
-    version='0.3.4',
-    description='Byte-pair embeddings in 275 languages',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://nlp.h-its.org/bpemb',
-    project_urls = {
-        "Source Code": "https://github.com/bheinzerling/bpemb",
-        "Bug Tracker": "https://github.com/bheinzerling/bpemb",
-    },
-    author='Benjamin Heinzerling',
-    author_email='benjamin.heinzerling@h-its.org',
-    license='MIT',
-    packages=['bpemb'],
-    install_requires=[
-        "gensim",
-        "numpy",
-        "requests",
-        "sentencepiece",
-        "tqdm"],
-    zip_safe=True,
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3"
-    ])
+if __name__ == "__main__":
+    setup()
