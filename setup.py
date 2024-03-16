ec85774945ca76dd93c1d9b4af2090e80c5779dc
Benjamin Heinzerling <benjamin.heinzerling@riken.jp>
2024-03-17 07:37:53 +0900

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name='bpemb',
-    version='0.3.4',
+    version='0.3.5',
     description='Byte-pair embeddings in 275 languages',
     long_description=long_description,
     long_description_content_type="text/markdown",
